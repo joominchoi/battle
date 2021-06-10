21501c6ec4bf9e86d4cd15c659bafb3b113fee73
JooMin Choi <joominchoi23@gmail.com>
2021-06-10 15:29:15 +0100

    test helpers - added web helpers and refactored

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,23 +1,14 @@
 feature 'Enter names' do
   scenario 'submitting names' do
-    visit('/')
-    fill_in :player_1_name, with: 'Charmander'
-    fill_in :player_2_name, with: 'Squirtle'
-    click_button 'Submit'
-
+    sign_in_and_play
     save_and_open_page # will save the web page and open the browser to display it
-    
     expect(page).to have_content 'Charmander v Squirtle'
   end
 end
 
 feature 'View HP' do
   scenario 'see Player 2 HP' do
-    visit('/')
-    fill_in :player_1_name, with: 'Charmander'
-    fill_in :player_2_name, with: 'Squirtle'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Squirtle: 100HP'
   end
 end
-
