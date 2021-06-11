47b3a18663b3b8a0f6a96a0843040378050b15aa
JooMin Choi <joominchoi23@gmail.com>
2021-06-11 11:40:37 +0100

    updated tests and moved OK link to correct location

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -2,7 +2,7 @@ feature 'Attacking' do
   scenario 'attacking player 2 and getting confirmation' do
   sign_in_and_play
   click_button 'Attack'
-  expect(page).to have_content 'Charmander attacked Squirtle!'
+  expect(page).to have_content 'Charmander attacked Squirtle: 90HP'
   end
   
   # scenario 'reduce players HP' do
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -9,8 +9,8 @@ feature 'Attacking' do
   scenario 'reduce player 2 HP by 10' do
     sign_in_and_play
     click_button 'Attack'
-    # click_link 'OK'
+    click_link 'OK'
     expect(page).not_to have_content 'Squirtle: 100HP'
-    expect(page).to have_content 'Squirtle!: 90HP'
+    expect(page).to have_content 'Squirtle: 90HP'
   end
 end
